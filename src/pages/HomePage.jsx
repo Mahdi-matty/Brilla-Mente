e6405e87f1ce263de623bae3544909f46215d4b9
mahdi <mmiq69@gmail.com>
2024-01-12 15:15:11 -0500

    basic react forms

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,56 @@
-import { useState, useEffect } from 'react';
-export default function HomePage() {
+import { useState } from 'react';
+import './style.css';
 
-    const [repos, setRepos] = useState([]);
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const  data = await API.getRepository();
-          console.log(data)
-          setRepos(data || []); // Ensure data is not undefined
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      fetchData();
-    }, []);
-  
-    return (
-      <div className="container pt-4">
-        <ul className="list-group list-group">
-          {repos.map((repo) => (
-            <ListItem key={repo.id}>
-              <Profile repo={repo} />
-            </ListItem>
-          ))}
-        </ul>
-        <Footer/>
-      </div>
-    );
-  }
+function HomePage() {
+  // Here we set two state variables for firstName and lastName using `useState`
+  const [userName, setuserName] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+    return name === 'username' ? setuserName(value) : setPassword(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // Alert the user their first and last name, clear the inputs
+   
+    setuserName('');
+    setPassword('');
+  };
+
+  return (
+    <div className="container text-center">
+      <h1>
+      login!
+      </h1>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={userName}
+          name="username"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="userName"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="password"
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+      <signup/>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/signup.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { checkPassword, validateEmail } from '../utils/helper';
+
+const signup = () => {
+    const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password, setPassword]=useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    }else if (inputType === 'password'){
+      setPassword(inputValue)
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email or username is invalid');
+      return;
+    }
+    if (!checkPassword(password)) {
+      setErrorMessage(
+        `Choose a more secure password for the account: ${userName}`
+      );
+      return;
+    }
+
+    setUserName('');
+    setPassword('');
+    setEmail('');
+    alert(`Hello ${userName}`);
+  };
+  return (
+    <footer>
+      <div className='githubRes'>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={userName}
+          name="username"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="userName"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="password"
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+        </div>
+    </footer>
+  );
+};
+
+export default signup;
