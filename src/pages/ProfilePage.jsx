1b110c1543a1f9553dc82fb9cc4015cb323ee855
mahdi <mmiq69@gmail.com>
2024-01-14 16:15:10 -0500

    added some style and items to profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,22 +1,33 @@
 import { Link } from 'react-router-dom';
+import { FaFileAlt, FaBook } from 'react-icons/fa';
  function ProfilePage() {
 
 
 
     return (
         <div>
-            <Link
-              to={`/myPosts`}
-              className="badge bg-primary rounded-pill"
-            >
-              Posts
-            </Link>
-            <Link
-              to={`/assignments`}
-              className="badge bg-primary rounded-pill"
-            >
-              Assignments
-            </Link>
+          <nav className='sideNav'>
+            <ul>
+              <li>
+                 <Link
+                  to={`/myPosts`}
+                  className="badge bg-primary rounded-pill"
+                  >
+                     <FaFileAlt className="iconSize"/> {/* Icon for Posts */}
+                     <span className='navSpan'>Posts</span>
+                </Link>
+              </li>
+              <li>
+                 <Link
+                    to={`/assignments`}
+                    className="badge bg-primary rounded-pill"
+                  >
+                     <FaBook className="iconSize"/> {/* Icon for Assignments */}
+                     <span className='navSpan'>Assignments</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>   
         </div>
     )
 }
