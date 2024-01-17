9e3cec14b55a7df6e525e033eb6c127f3ed7283e
mahdi <mmiq69@gmail.com>
2024-01-17 14:43:36 -0500

    quiz finished

---
+++ src/components/UI/exam.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react"
+
+export default function Exam (){
+    const [score, setScore] = useState('');
+    const [question, setQuestion] = useState(1);
+    const [timer, setTimer] = useState(30);
+    const [isAnswered, setIsAnswered] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+    
+        return () => clearInterval(interval);
+      }, []);
+    
+      useEffect(() => {
+        if (timer === 0) {
+            setIsAnswered(true);
+            setQuestion((prevQuestion) => prevQuestion + 1);
+            setTimer(30);
+          }
+        }, [timer]);
+      
+        const handleAnswer = (isCorrect) => {
+          if (!isAnswered) {
+            setIsAnswered(true);
+            setQuestion((prevQuestion) => prevQuestion + 1);
+      
+            if (isCorrect) {
+              setScore((prevScore) => prevScore + 5);
+            }
+      
+            setTimer(30);
+          }
+        };
+    return (
+    <>
+    <div>
+        <p>Question {question}</p>
+        <p>Time left: {timer} seconds</p>
+        {!isAnswered && (
+          <>
+            <button onClick={() => handleAnswer(true)}>Correct</button>
+            <button onClick={() => handleAnswer(false)}>Incorrect</button>
+          </>
+        )}
+      </div>
+      <div>
+        <p>Score: {score}</p>
+        {question <= 10 && <button onClick={() => setIsAnswered(false)}>Next Question</button>}
+      </div>
+    </> 
+    )
+   
+}
--- src/pages/quizPage.jsx
+++ src/pages/quizPage.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react"
+import Exam from "../components/UI/exam";
 function QuiztPage(){
+    const [subject, setSubject] = useState('');
+    const [isSelected, setIsSelected] = useEffect(false)
+    // we need fetch request to the specific subject elected by student
     return (
-        <div></div>
+        <>
+        {isSelected?(
+            <div>
+                <Exam />
+            </div>
+        ):(
+            <div>
+                <select value={subject} onChange={e=> setSubject(e.target.value)}></select>
+            </div>
+        )
+       
+        }
+       
+        </>
+        
     )
 }
 
