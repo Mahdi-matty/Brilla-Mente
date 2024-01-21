a439c1f2d82375cc969f1e9e034cc9862f64b853
mahdi <mmiq69@gmail.com>
2024-01-21 13:27:39 -0500

    added share

--- src/pages/AssignmentPages.jsx
+++ src/pages/AssignmentPages.jsx
@@ -1,5 +1,25 @@
 import YoutubeEmbed from "../components/youtubetest/youtube"
 import React from "react"
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  MailruShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+
+  EmailIcon,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  LinkedinIcon,
+  MailruIcon,
+  RedditIcon,
+  TelegramIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share";
 
 function AssignmentPage(){
     const youtubeUrl = "https://www.youtube.com/watch?v=cX8Z7BEq25A";
@@ -8,6 +28,10 @@ function AssignmentPage(){
       <div>
         <h1>My React App</h1>
         <YoutubeEmbed url={youtubeUrl} />
+        <h3>what is going on?</h3>
+        <WhatsappShareButton url={'http://localhost:3000/assignments'}>
+          <WhatsappIcon />
+        </WhatsappShareButton>
       </div>
     );
   };
