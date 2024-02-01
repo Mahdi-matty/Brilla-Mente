eec4e303246decdb5e009668ab301b7b919aaee8
mahdi <mmiq69@gmail.com>
2024-02-01 18:12:46 -0500

    accept card

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,67 @@
 import React from 'react';
 import BrillaLogo from '../assets/brillam.png'
 import {FaBell} from 'react-icons/fa'
+import { useState, useEffect } from 'react';
 
 const Footer = () => {
+
+   // const URL_PREFIX="https://brilla-back-fb4c71e750bd.herokuapp.com"
+   const URL_PREFIX = "http://localhost:3001"
+   const token = localStorage.getItem('token')
+
+   const [pendingcards, setPendingCards] = useState([])
+   const [showAcceptPopup, setShowAcceptPopup] = useState(false);
+
+   useEffect(()=>{
+    fetch(`${URL_PREFIX}/api/cards/find-pending`,{
+      headers:{
+        Authorization:`Bearer ${token}`
+      }
+    }).then(res=>res.json()).then(data=>{
+      console.log('data', data)
+      setPendingCards(data)
+      console.log(pendingcards)
+    }).catch(error => console.log(error))
+  },[token])
+  const acceptCard = async(e)=>{
+    e.preventDefault();
+    try {
+      const response = await fetch(`${URL_PREFIX}/api/cards/:cardId/:topicId`, {
+        method:"PUT",
+      });
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage || 'Something went wrong!');
+      }
+  
+    } catch(error){
+      console.log(error)
+    }
+  }
+  const showPending = () =>{
+    setShowAcceptPopup(!showSharePopup)
+  }
+  
   return (
+    <>
     <footer>
       <div className='notficationDiv'>
-          <p><FaBell className="iconSize"/>Notification</p>
+          <button><FaBell onClick={()=>showPending(card)} className="iconSize"/></button>
+        {showAcceptPopup && (
+        <div>
+          <p>pending Cards</p>
+          {pendingcards.map((card, index)=>{
+            <li key={index} className='pendingCards'>
+              <p>{card.title}</p>
+              <button onClick={(e)=>acceptCard(e)}>Accespt</button>
+            </li>
+          })}
         </div>
+         )}
+         </div>
     </footer>
+    </>
+    
   );
 };
 
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,14 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { FaFileAlt, FaBook, FaQuestionCircle } from 'react-icons/fa';
-import Footer from '../components/footer'
+import { FaFileAlt, FaBook, FaQuestionCircle, FaBell } from 'react-icons/fa';
 import { useState, useEffect } from 'react';
  function ProfilePage() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const token =  localStorage.getItem('token')
   const navigate = useNavigate()
+   // const URL_PREFIX="https://brilla-back-fb4c71e750bd.herokuapp.com"
+   const URL_PREFIX = "http://localhost:3001"
+   const [pendingcards, setPendingCards] = useState([])
+   const [showAcceptPopup, setShowAcceptPopup] = useState(false);
 
   useEffect(() => {
     const savedToken = localStorage.getItem('token');
@@ -13,6 +16,17 @@ import { useState, useEffect } from 'react';
       setIsLoggedIn(true);
     }
   }, []);
+  useEffect(()=>{
+    fetch(`${URL_PREFIX}/api/cards/find-pending`,{
+      headers:{
+        Authorization:`Bearer ${token}`
+      }
+    }).then(res=>res.json()).then(data=>{
+      console.log('data', data)
+      setPendingCards(data)
+      console.log(pendingcards)
+    }).catch(error => console.log(error))
+  },[token])
 
   if (!isLoggedIn) {
     return (
@@ -21,6 +35,24 @@ import { useState, useEffect } from 'react';
         <Link to="/">Back to Home</Link>
       </div>
     );
+  }
+  const acceptCard = async(e, cardId, topicId)=>{
+    e.preventDefault();
+    try {
+      const response = await fetch(`${URL_PREFIX}/api/cards/${cardId}/${topicId}`, {
+        method:"PUT",
+      });
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage || 'Something went wrong!');
+      }
+  
+    } catch(error){
+      console.log(error)
+    }
+  }
+  const showPending = () =>{
+    setShowAcceptPopup(!showAcceptPopup)
   }
 
     return (
@@ -57,9 +89,24 @@ import { useState, useEffect } from 'react';
                      <span className='navSpan'>Quiz</span>
                 </Link>
               </li>
+              <li>
+                <button onClick={()=>showPending()}><FaBell className="iconSize"/></button>
+                {showAcceptPopup && (
+            <div>
+            <p>pending Cards</p>
+            <ul>
+          {pendingcards.map((card)=>(
+            <li key={card.id} className='pendingCards'>
+              <p>{card.title}</p>
+              <button onClick={(e)=>acceptCard(e, card.id, card.topic)}>Accespt</button>
+            </li>
+          ))}
+          </ul>
+        </div>
+         )}</li>
+         
             </ul>
           </nav> 
-          <Footer />
         </div>
     )
 }
